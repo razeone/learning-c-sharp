237abe19a3a94c1aa5734bae986ed1a4098402ba
Jorge Alberto Alcaraz Reyes <jorgealc@microsoft.com>
2022-09-26 13:42:35 -0500

    Finish strings lesson

--- Strings/ProgChallengeStart/Program.cs
+++ Strings/ProgChallengeStart/Program.cs
@@ -15,6 +15,31 @@ namespace ProgChallengeStart
             Console.WriteLine("Enter your guess, or -1 to give up.");
 
             // Keep track of the number of guesses and the current user guess
+            string guessStr;
+            bool correctAnswer = false;
+
+            while(correctAnswer == false) {
+                guessStr = Console.ReadLine();
+                if(guessStr == "-1") {
+                    Console.WriteLine("You gave up!");
+                    break;
+                }
+                if(int.TryParse(guessStr, out int guess)) {
+                    if(guess == theNumber) {
+                        Console.WriteLine("You guessed it!");
+                        correctAnswer = true;
+                    }
+                    else if(guess < theNumber) {
+                        Console.WriteLine("Too low!");
+                    }
+                    else {
+                        Console.WriteLine("Too high!");
+                    }
+                }
+                else {
+                    Console.WriteLine("Invalid input!");
+                }
+            }
 
             // Start the game and run until user quits or guesses correctly
             // HINT: You'll need a way to convert the user's input to an integer
